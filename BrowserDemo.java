1495fa7b80c8be80e0f28ebf57595da357e43d52
Sethulakshmi M S <96937183+Sethums@users.noreply.github.com>
2023-02-23 11:31:48 +0530

    Add files via upload

--- BrowserDemo.java
+++ BrowserDemo.java
@@ -1,63 +1,116 @@
 package Exercise;
-
 import java.util.Scanner;
 import java.util.ArrayList;
 import java.util.List;
 
-class Browser {
-//	String br[];
+class Browser 
+{//method1
+	interface Short{
+		public void dis();
+	}
+//method 2
+    public class Shortcut1
+    {
+    	void details() 
+    	{
+    		System.out.println("Inside Anonymous class1");
+    	}
+    }
+    static int a;
+	void addShortcuts()//for invoking anonymous inner class Shortcuts
+	{
+		Shortcuts sobj=new Shortcuts();
+		sobj.display();
+	}
+	 class Shortcuts//Anonymous inner class
+	{
+		void display() 
+		{
+	    System.out.println("Inside Anonymous class");
+		}
+	}
+	class Bookmarks//Member Inner class
+	{
+		void display(ArrayList<String> book1)
+		{System.out.print("ArrayList: ");
+	    for(String b :book1)
+	    {
+	        System.out.println(b);
+	      }
+		}
+	}
+	static class History
+	{
+		void display(int a1)
+		{
+			a=a1;
+			System.out.println("Value of a inside static Inner class"+a);
+		}
+
+	}
+//	static variable- single copy for all objects
 	private static String hist[];
 
-//	public void setBr(String[] br) {
-//		this.hist = br;
-//	}
-
-	public static String[] getHist() {
+	public String[] getHist() 
+	{
 		return hist;
 	}
-
-	public static void setHist(String[] hist) {
-		Browser.hist = hist;
+//non static fields or methods cannot use inside static method.
+	public static void setHist(String[] hist)
+	{
+		Browser.hist = hist;//this.hist cannot use in static context
 	}
 
-	Browser() {
-
+	Browser() 
+	{
+     //empty constructor
 	}
 
-	Browser(String[] br1) {
+	Browser(String[] br1)
+	{
 		this.hist = br1;
 	}
 
-
-
-	
-	
 }
-public class BrowserDemo {
-	public static void main(String args[]) {
+public class BrowserDemo 
+{
+	public static void main(String args[]) 
+	{
 		Scanner sc = new Scanner(System.in);
 		
 		
-		int n;
+		int n1,n;
 		ArrayList<String> list = new ArrayList<String>();
-		
-        System.out.println("Enter URLs:");
-           String st = sc.nextLine();
-            
-            
-            list.add(st+" ");
-            
-        
+		System.out.println("Enter URLs:");
+        String st = sc.nextLine();
+        list.add(st+" ");
         n=list.size();
-		  String arr[]=new String[n];
-		  String arr1[]=new String[n];
-		  String arr2[]=new String[n];
-		  for (int i = 0; i <list.size(); i++)
-	            arr[i] = list.get(i);
-		Browser chrome = new Browser();
-		chrome.setHist(arr);
+       
+        Browser bobj=new Browser();
+		Browser.Bookmarks mobj=bobj.new Bookmarks();
+		mobj.display(list);
+		Browser.Shortcut1 s= bobj.new Shortcut1();
+		s.details();
+		bobj.addShortcuts();
+		Browser.History hobj=new Browser.History();
+		hobj.display(8);
+		Browser.Short s1=new Browser.Short() {
+		public void dis() {
+			System.out.println("Anonymous inner class demo");
+		}
+		};
+		s1.dis();
+        n1=list.size();
+		String arr[]=new String[n];
+		String arr1[]=new String[n];
+		String arr2[]=new String[n];
+		  
 		
-		Browser edge = new Browser(arr);
+		
+		Browser chrome = new Browser();
+		chrome.setHist(new String[]{"chrome"});
+		
+		Browser edge = new Browser(new String[] {"edge"});
 		
 		
 		arr1=chrome.getHist();
